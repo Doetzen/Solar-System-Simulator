6d67b93f523b7b56d0708d6a4f6bed011799d85b
HvtEnde <97097945@st.deltion.nl>
2026-02-03 10:07:55 +0100

    [ADD] Import TMP, Start Game Script
    - Imported TMP for text components
    - Created and written option to start game

---
+++ Assets/Scripts/Hannah/UI Scripts/ToGame.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToGame : MonoBehaviour
+{
+    public void ToMainGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+    
