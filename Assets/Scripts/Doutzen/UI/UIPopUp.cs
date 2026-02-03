6ac259f24aeaf3cef58005df4fb873362ee65abe
Doetzen <Doutzenhoekstra06@gmail.com>
2026-02-03 14:21:55 +0100

    [CHANGE] new planet prefab
    [CHANGE] planet scales when on hovered
    [CHANGE] UI fades in and out
    [ADD] Lookat camera script for UI

--- Assets/Scripts/Doutzen/UI/UIPopUp.cs
+++ Assets/Scripts/Doutzen/UI/UIPopUp.cs
@@ -1,31 +1,70 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIPopUp : MonoBehaviour
 {
-    [SerializeField] private GameObject _objectToEnable;
+    [SerializeField] private CanvasGroup _objectToEnable;
 
     [SerializeField] private AudioSource _popUpAudio;
     [SerializeField] private AudioSource _hoverAudio;
 
-    //something for hover shader
+    [SerializeField] private Transform _objectToScale;
+
+    [SerializeField] private Vector3 _selectedScale;
+    //variable for hover shader
+
     private void OnMouseEnter()
     {
         // Enable UI gameObject
         // in enter play popup audio
         // start hoveraudio
-        _objectToEnable.SetActive(true);
+        StopAllCoroutines();
+        StartCoroutine(FadeCVGroupOn(1, 2));
+
+        if( _objectToScale != null)
+        {
+            _objectToScale.localScale += _selectedScale;
+        }
+
         //set hover shader value to on
-        Debug.Log("ONMOUSEENTER");
     }
 
     private void OnMouseExit()
     {
         //disable UI gameobject
         //stop hoveraudio
-        _objectToEnable.SetActive(false);
+        StopAllCoroutines();
+        StartCoroutine(FadeCVGroupOff(0, 2));
+
+        if ( _objectToScale != null )
+        {
+            _objectToScale.localScale -= _selectedScale;
+        }
 
         // set hover shader value to off
+    }
+    private IEnumerator FadeCVGroupOn(float toFadeTo, float speed)
+    {
+        float current = _objectToEnable.alpha;
 
-        Debug.Log("ONMOUSEEXIT");
+        while (current < toFadeTo)
+        {
+            current += Time.deltaTime * speed;
+            _objectToEnable.alpha = current;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeCVGroupOff(float toFadeTo, float speed)
+    {
+        float current = _objectToEnable.alpha;
+
+        while (current > toFadeTo)
+        {
+            current -= Time.deltaTime * speed;
+            _objectToEnable.alpha = current;
+            yield return null;
+        }
     }
 }
