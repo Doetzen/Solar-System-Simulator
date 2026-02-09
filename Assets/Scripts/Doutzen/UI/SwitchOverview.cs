aa03c12bd44df46f7e8fa8258453e75324b554c9
Doetzen <Doutzenhoekstra06@gmail.com>
2026-02-09 15:28:02 +0100

    [CHANGE] Camera moves to new target instead of snapping

--- Assets/Scripts/Doutzen/UI/SwitchOverview.cs
+++ Assets/Scripts/Doutzen/UI/SwitchOverview.cs
@@ -1,8 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwitchOverview : MonoBehaviour
 {
     private bool _standartViewOn;
+    private bool _isSwitching;
+
+    [SerializeField] private float _speed = 5;
 
     [SerializeField] private GameObject[] _planets;
     [SerializeField] private GameObject _camHolder;
@@ -14,20 +18,35 @@ public class SwitchOverview : MonoBehaviour
     {
         _standartViewOn = true;  
     }
+    private void Update()
+    {
+        if (_standartViewOn && _isSwitching)
+        {
+            MoveCam(_nearCamPos, _speed);
+            Debug.Log("Move NEAR?");
 
+        }
+        else if (!_standartViewOn && _isSwitching) 
+        {
+            MoveCam(_farCamPos, _speed);
+            Debug.Log("Move FAR?");
+        }
+    }
     public void SwitchView()
     {
+        _isSwitching = true;
+
         if (_standartViewOn)
         {
             _standartViewOn = false;
-            _camHolder.transform.position = _farCamPos.position;
-            _camHolder.transform.rotation = _farCamPos.rotation;
+
+           // _camHolder.transform.rotation = _farCamPos.rotation;
         }
         else
         {
             _standartViewOn = true;
-            _camHolder.transform.position = _nearCamPos.position;
-            _camHolder.transform.rotation = _nearCamPos.rotation;
+
+            //_camHolder.transform.rotation = _nearCamPos.rotation;
 
         }
 
@@ -36,4 +55,15 @@ public class SwitchOverview : MonoBehaviour
             planet.GetComponent<MovePlanet>().Switch(_standartViewOn);
         }
     }
+
+    private void MoveCam(Transform newPos, float speed)
+    {
+        //Vector3 targetPos = new Vector3(newPos.position.x, newPos.position.y, newPos.position.z);
+        //_camHolder.transform.position += (targetPos - transform.position).normalized * speed * Time.deltaTime;
+        _camHolder.transform.position = Vector3.MoveTowards(_camHolder.transform.position, newPos.position, speed);
+        if(_camHolder.transform.position == newPos.position)
+        {
+            _isSwitching = false;
+        }
+    } 
 }
