329c8a5fd2402f3e4cb86bf68f2b8ee89d97ffcb
Doetzen <Doutzenhoekstra06@gmail.com>
2026-02-09 09:59:05 +0100

    [ADD] Loading scene

---
+++ Assets/Scripts/Doutzen/SwitchScene/SwitchSceneAfterWormhole.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneAfterWormhole : MonoBehaviour
+{
+    private string _sceneToSwitchTo;
+
+    private void Start()
+    {
+        _sceneToSwitchTo = PlayerPrefs.GetString("sceneToLoad");
+        Debug.Log("Loading: " + _sceneToSwitchTo);
+
+        StartCoroutine(TimeToWait());
+
+    }
+
+    private IEnumerator TimeToWait()
+    {
+
+        AsyncOperation sceneOperation = SceneManager.LoadSceneAsync(_sceneToSwitchTo);
+
+        while(!sceneOperation.isDone)
+        {
+            //for if we want a loading slider
+            //float progress = Mathf.Clamp01(sceneOperation.progress / 0.9f);
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/Doutzen/UI/SwitchOverview.cs
+++ Assets/Scripts/Doutzen/UI/SwitchOverview.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Splines;
 
 public class SwitchOverview : MonoBehaviour
 {
